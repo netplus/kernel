078317e415f40b06808c3f9165d45f4b57e09217
copy <netplusplus@outlook.com>
2026-08-06 11:06:25 +0800

    lab: add compiler comparison C example

---
+++ assembly/labs/01-register-width/companion.c
@@ -0,0 +1,16 @@
+#include <stdint.h>
+
+uint64_t zero_extend_u32(uint32_t x)
+{
+    return x;
+}
+
+int64_t sign_extend_i32(int32_t x)
+{
+    return x;
+}
+
+uint64_t add_u64(uint64_t a, uint64_t b)
+{
+    return a + b;
+}
